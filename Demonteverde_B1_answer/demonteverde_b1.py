391e3c18af7c55caebe2eac2695d95756044ff38
Azad Demonteverde <nbdemonteverde@up.edu.ph>
2023-12-04 11:47:33 +0800

    trial

--- Demonteverde_B1_answer/demonteverde_b1.py
+++ Demonteverde_B1_answer/demonteverde_b1.py
@@ -1,31 +1,6 @@
-def count_species(Exam_Table):
-
-    file_content = []                       
-    dictionary = {}                         
-
-    file = open(file_name, "r")             
-    content = file.readlines()              
-
-    # print(content)
-    for i in range(len(content)):           
-        line = content[i].rstrip()          
-        file_content.append(line)    
-
-    file.close()
-
-    for word in file_content:
-        if word in dictionary.keys():       
-            dictionary[word] += 1
-        else:                              
-            dictionary.update({word: 1})
-    
-    print(dictionary)
-
-    with open("Exam_Table.csv", "w") as file:
-        for word, count in dictionary.items():
-            file.write("%s %s\n" % (word, count))
-
-file_name = "Exam_Table.csv"
-count_species(Exam_Table)
-
+import pandas as pd
+df = pd.read_csv("Exam_Table.csv")
+num_rows_to_extract = 31
+extracted_rows = df.head(31)
+print (extracted_rows)
     
