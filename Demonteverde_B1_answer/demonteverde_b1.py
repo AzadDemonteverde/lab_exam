08cbf50d24fa38f13f0340d1d732d24c8f11a187
Azad Demonteverde <nbdemonteverde@up.edu.ph>
2023-12-04 11:53:07 +0800

    documentation

--- Demonteverde_B1_answer/demonteverde_b1.py
+++ Demonteverde_B1_answer/demonteverde_b1.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv("Exam_Table.csv")
-selected_rows = df.loc[:30]
-print (selected_rows)
+df = pd.read_csv("Exam_Table.csv") #added reading line
+selected_rows = df.loc[:30] #selected rows from 0 to 30
+print (selected_rows) #printing selected rows 0 to 30
     
