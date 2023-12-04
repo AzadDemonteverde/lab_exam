c2f7b4de3166cb30bf04fc9804860fffc48d0898
Azad Demonteverde <nbdemonteverde@up.edu.ph>
2023-12-04 12:34:04 +0800

    final

--- Demonteverde_B5_answer/demonteverde_b5.py
+++ Demonteverde_B5_answer/demonteverde_b5.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv("Exam_Table.csv")
-pa_rows = df[df['Species'] == 'Pomacentrus adelus']
+pa_rows = df[df['Species'] == 'adelus']
 average_count = pa_rows['Count'].mean()
 
-print ("Rows related to Pomacentrus adelus")
+print ("Rows related to 'adelus'")
 print ("pa_rows")
 print ("\nAverage Count:", average_count)
